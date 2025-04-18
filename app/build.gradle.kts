e867de403271a4ca045e44e4fddc2a257ac52a39
emreargana <emrewindowsarg@gmail.com>
2025-04-18 19:37:26 +0300

    Refactor: Update dependency versions and add Hilt Work support
    This commit updates the `libs.versions.toml` file to include new dependency versions.
    It introduces `hiltWork` and `workRuntimeKtx` version entries, and adds new library entries for `androidx-hilt-compiler`, `androidx-hilt-work`, and `androidx-work-runtime-ktx`.
    Also it includes `androidx.hilt.work` and `androidx.work.runtime.ktx` dependencies in `build.gradle.kts` with related ksp.
    These changes add Hilt Work support to the project.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.work.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
@@ -62,6 +63,8 @@ dependencies {
 
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.work)
+    ksp(libs.androidx.hilt.compiler)
 
     implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
